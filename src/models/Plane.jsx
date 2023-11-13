46a8668c79af2b29ebbc3f6c709ac04d4869dd5d
Sumit <marvelsumit2002@gmail.com>
2023-11-13 16:51:36 +0530

    second commit

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { useRef, useEffect } from 'react'
+import { useAnimations, useGLTF } from '@react-three/drei'
 
-const Plane = () => {
+import planeScene from '../assets/3d/plane.glb'
+
+const Plane = ({ isRotating, ...props }) => {
+  const ref = useRef()
+  const { scene, animations } = useGLTF(planeScene)
+  const { actions } = useAnimations(animations, ref)
+
+  useEffect(()=> {
+    if(isRotating) {
+      actions['Take 001'].play()
+    } else {
+      actions['Take 001'].stop()
+    }
+  }, [actions, isRotating])
+
   return (
-    <mesh>Plane</mesh>
+    <mesh {...props} ref={ref}>
+      <primitive object={scene} />
+    </mesh>
   )
 }
 
